9378c7b19b1ae2153b8479b75b5e51ecd8fed81a
Samiraa <samiradg105@gmail.com>
2024-08-09 14:51:43 +0000

    Merged in Stock (pull request #1)

--- src/main/java/com/example/backend/model/Account.java
+++ src/main/java/com/example/backend/model/Account.java
@@ -1,4 +1,15 @@
 package com.example.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Account {
+    @Id
+    private String nameCode;
+
+    @ManyToMany
+    private Stock stock;
+
 }
